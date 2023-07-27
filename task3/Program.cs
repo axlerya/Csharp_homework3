40f6b5202d968f25f1c333b8e4778bee5088ac30
axlerya <white_cat_02@mail.ru>
2023-07-28 04:46:01 +0500

    feat: add task 3
    add a solution to the third task

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.WriteLine("Введите число");
+int numberInput = Convert.ToInt32(Console.ReadLine());
+
+void arrayNumbersSequence(int num)
+{
+    int k = 1;
+    int j = 1;
+    int[] arrayNumbers = new int[num];
+    for (int i = 0; i < arrayNumbers.Length; i++) arrayNumbers[i] = k++;
+    double[] arrayNumbersCube = new double[num];
+    for (int i = 0; i < arrayNumbers.Length; i++) arrayNumbersCube[i] = Math.Pow(j++,3);
+    Console.WriteLine(String.Join(", ", arrayNumbersCube));
+}
+
+arrayNumbersSequence(numberInput);
